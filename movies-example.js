963b74848fa072e7ee09684f57563ce7dabc6c9d
Shai Fisher <shai.fisher@gmail.com>
2018-10-06 20:41:42 +0300

    merge

---
+++ movies-example.js
@@ -0,0 +1,12 @@
+var movies = [
+	{'name' : 'My Neighbor Totoro', 'video': 'Totoro.mp4'},
+	{'name' : 'Wallace and Gromit - A Grand Day Out', 'video': 'Wallace and Gromit\\Wallace and Gromit - A Grand Day Out.mp4'},
+	{'name' : 'Wallace and Gromit - A Close Shave', 'video': 'Wallace and Gromit\\Wallace and Gromit - A Close Shave.mp4'},
+	{'name': 'Alfred J Quack', type: 'series', 'episodes': [
+		{'name': 'Chapter 1', 'video': 'Alfred\\Alfred.e01.mp4'},
+		{'name': 'Chapter 2', 'video': 'Alfred\\Alfred.e02.mp4'},
+		{'name': 'Chapter 3', 'video': 'Alfred\\Alfred.e03.mp4'},
+	]},
+	{'name': 'Pink Panther - Secret Spy', 'type': 'youtube', 'id': 'plgpkJJeKpw'},
+];
+
